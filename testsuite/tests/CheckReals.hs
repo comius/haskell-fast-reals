4ea50a724059091bef3656711f7e9fda53591ef6
Ivo List <ivo.list@guest.arnes.si>
2017-09-03 15:37:26 +0200

    Failing estimates.

--- testsuite/tests/CheckReals.hs
+++ testsuite/tests/CheckReals.hs
@@ -1,13 +1,17 @@
 
 import Test.HUnit
 
+import Debug.Trace
+import Data.Ratio
 import Data.Approximate.Floating.MPFR
+import Data.Approximate.Interval
 import Data.Approximate.ApproximateField
 import Data.Reals.Reals
 import Data.Reals.Space
+import Data.Reals.Staged
 import Test.Framework
 import Test.Framework.Providers.HUnit
-import Test.Framework.Providers.QuickCheck
+import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
 
 assertIn a b x = assertBool 
@@ -40,24 +44,37 @@ test_2 = TestCase (do
       a = forall i $ \x -> (x * (1 - x)) `less` (0.26) :: Estimate
       b = forall i $ \x -> (x * (1 - x)) `less` (0.24) :: Estimate
       
-prop_RevRev xs = reverse (reverse xs) == xs
-  where types = xs::[Int]
-     
-test_4 = TestCase (do
-                quickCheck prop_RevRev          
-        )
-      
-{-test_3 = TestCase (do
-      assertBool "forall x:[0,1].x*(1-x)<0.26" (force a) 
-      assertBool "forall x:[0,1].x*(1-x)<0.24" (not $ force b)
-      )
--}
+inside x [] = False      
+inside x (l:ls) = (x >= (Data.Approximate.Interval.lower l) && x <= (Data.Approximate.Interval.upper l)) || inside x ls
 
+i = ClosedInterval (appFromInteger 0 :: Rounded, appFromInteger 1)
+
+prop_pos1 x = xr `inside` (Data.Reals.Staged.upper e) ==> (f x :: Bool)
+  where 
+      types = x :: (Ratio Integer)
+      xr = appFromRational (precUp 52) x :: Rounded
+      e = approximate (estimate f i :: Estimate) 52
+      f x = (x * (1 - x)) `less` (0.4)
+
+prop_pos2 x = xr `inside` (Data.Reals.Staged.upper e) ==> (f x :: Bool)
+  where 
+      types = x :: (Ratio Integer)
+      xr = appFromRational (precUp 52) x :: Rounded
+      e = approximate (estimate f i :: Estimate) 52
+      f x = x `less` (0.5)
+
+prop_pos3 x = xr `inside` (Data.Reals.Staged.upper e) ==> (f x :: Bool)
+  where 
+      types = x :: (Ratio Integer)
+      xr = appFromRational (precUp 52) x :: Rounded
+      e = approximate (estimate f i :: Estimate) 52
+      f x = (-x) `less` (-0.5)
+     
 --tests :: Test
 tests = hunit ++ qcheck
     where
-       hunit = hUnitTestToTests $ TestList [TestLabel "test1" test_1, TestLabel "test2" test_2]--, TestLabel "qtest" test_4]
-       qcheck = [testProperty "revrev" prop_RevRev]
+       hunit = hUnitTestToTests $ TestList [TestLabel "test1" test_1, TestLabel "test2" test_2]
+       qcheck = [testProperty "pos1" prop_pos1, testProperty "pos2" prop_pos2, testProperty "pos3" prop_pos3]
 
 --main :: IO Counts
 --main = do runTestTT tests
